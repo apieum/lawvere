21af62ccf866bb39b885492a5afa69a2e821fb8d
Grégory Salvan <apieum@gmail.com>
2014-08-15 12:59:57 +0200

    Added domain and codomain checking in morphism

--- lawvere/morphism.py
+++ lawvere/morphism.py
@@ -3,7 +3,52 @@ from .curry import Curry
 from .signatures import use_signature, Signature
 
 class MorphismSignature(Signature):
-    pass
+    def __init__(self, args, kwargs, domain=tuple(), codomain=tuple()):
+        Signature.__init__(self, args, kwargs)
+        self.domain = domain
+        self.codomain = codomain
+
+    @classmethod
+    def from_func(cls, func):
+        domain = getattr(func, 'domain', tuple())
+        codomain = getattr(func, 'codomain', tuple())
+        signature = cls(*cls.inspect_parameters(func), domain=domain, codomain=codomain)
+        return signature
+
+    @property
+    def domain(self):
+        return self.__dict__.get('domain')
+
+    @domain.setter
+    def domain(self, domain):
+        if not isinstance(domain, tuple):
+            domain = (domain, )
+        self.__dict__['domain'] = domain
+
+
+    def check_domain(self):
+        if self.argcount != len(self.domain):
+            raise TypeError("Domain not valid")
+        index = 0
+        for name, arg in self.args:
+            if not isinstance(arg, self.domain[index]):
+                raise TypeError("Argument %s not in domain" % name)
+            index +=1
+
+    def check_codomain(self, result):
+        if not isinstance(result, self.codomain):
+            raise TypeError("Result not in codomain")
+
+    def apply(self, func):
+        self.check_domain()
+        result = func(**self)
+        self.check_codomain(result)
+        return result
+
+    def __copy__(self):
+        return type(self)(self.args, self.keywords, self.domain, self.codomain)
+
+
 
 @use_signature(MorphismSignature)
 class Morphism(Curry):
@@ -25,15 +70,15 @@ class Arrow(Mapping):
 class Domain(Mapping):
     def __init__(self, domain):
         self.domain = domain
-        self.codomain = None
+        self.codomain = tuple()
 
     def __rshift__(self, mapping):
-        mapping = getattr(mapping, 'codomain', mapping)
-        return Arrow(self.domain, mapping)
+        codomain = getattr(mapping, 'codomain', mapping)
+        return Arrow(self.domain, codomain)
 
 class Codomain(Mapping):
     def __init__(self, codomain):
-        self.domain = None
+        self.domain = tuple()
         self.codomain = codomain
 
 
--- lawvere/tests/testMorphism.py
+++ lawvere/tests/testMorphism.py
@@ -30,13 +30,33 @@ class MappingsTest(TestCase):
 
 class MorphismTest(TestCase):
     def test_it_can_curry(self):
+        morph = Arrow(int, int)
         func = lambda x, y=1, z=2: x + y + z
-        morph = Morphism(func)
+        morph = morph(func)
         add_5 = morph(z=3, y=2)
         self.assertEqual(10, add_5(5))
 
     def test_it_is_composable(self):
-        add = Morphism(lambda x, y: x + y)
-        sub = Morphism(lambda x, y: x - y)
+        morph = Arrow((int, int), int)
+        add = morph(lambda x, y: x + y)
+        sub = morph(lambda x, y: x - y)
         add_5_sub_2 = add(5) >> sub(y=2)
         self.assertEqual(10, add_5_sub_2(7))
+
+    def test_it_raises_type_error_if_domain_len_not_equals_args_len(self):
+        morph = Arrow(int, int)
+        add = morph(lambda x, y: x + y)
+        with self.assertRaises(TypeError) as context:
+            add(4, 2)
+
+    def test_it_raises_type_error_if_args_not_instance_of_domain(self):
+        morph = Arrow((str, int), int)
+        add = morph(lambda x, y: x + y)
+        with self.assertRaises(TypeError) as context:
+            add(4, 2)
+
+    def test_it_raises_type_error_if_result_not_instance_of_codomain(self):
+        morph = Arrow((int, int), str)
+        add = morph(lambda x, y: x + y)
+        with self.assertRaises(TypeError) as context:
+            add(4, 2)
