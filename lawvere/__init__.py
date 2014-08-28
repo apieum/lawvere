b19fe63534d62036b83c79af30a9629959566e95
Grégory Salvan <apieum@gmail.com>
2014-08-29 01:08:45 +0200

    Added compose_with_self in api

--- lawvere/__init__.py
+++ lawvere/__init__.py
@@ -2,7 +2,7 @@ from .morphism import Morphism
 from .curry import Curry
 from .arrow import ArrowType
 from .dispatcher import dispatch
-from .stack import composable, compose_with
+from .stack import composable, compose_with, compose_with_self
 
 morphism = ArrowType(Morphism)
 arrow = ArrowType(dispatch(Morphism))
