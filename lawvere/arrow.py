6e1ae1cebe1bca6e36232d8b9da00f2db5bb2734
Grégory Salvan <apieum@gmail.com>
2014-08-30 02:55:18 +0200

    Arrow use Curry

--- lawvere/arrow.py
+++ lawvere/arrow.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
-
+from .curry import Curry
 Default = type('Default', (tuple, ), {})()
 
-def Arrow(domain=Default, codomain=Default, func_type=lambda func: func):
+@Curry
+def annotate(domain, codomain, func):
+    varnames = getattr(func.__code__, 'co_varnames', tuple())
+    annotations = getattr(func, '__annotations__', {})
+    annotations.update(zip(varnames, domain))
+    if codomain is not Default:
+        annotations['return'] = codomain
+    setattr(func, '__annotations__', annotations)
+    return func
+
+
+@Curry
+def Arrow(domain, codomain=Default, func_type=lambda func: func):
     if type(domain) == type:
         domain = (domain, )
 
     if callable(domain):
         return func_type(domain)
 
-    def call(func):
-        varnames = getattr(func.__code__, 'co_varnames', tuple())
-        annotations = getattr(func, '__annotations__', {})
-        annotations.update(zip(varnames, domain))
-        if codomain is not Default:
-            annotations['return'] = codomain
-        setattr(func, '__annotations__', annotations)
-        return func_type(func)
-    return call
+    return lambda func: func_type(annotate(domain, codomain)(func))
+
+
+
 
 def ArrowType(cls):
-    return lambda domain=Default, codomain=Default: Arrow(domain, codomain, cls)
+    return Arrow(func_type=cls)
